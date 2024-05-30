74a5b4096b015c7808ff429525ad0c5ba01860e4
Lv777n <luanvitor.mat@gmail.com>
2024-05-30 13:20:33 -0300

    código 100% finalizado

--- Dicee+Challenge+-+Starting+Files/app.js
+++ Dicee+Challenge+-+Starting+Files/app.js
@@ -1,7 +1,7 @@
 let rndNumber1 = Math.ceil(Math.random() * 6);
 let rndNumber2 = Math.ceil(Math.random() * 6);
 let random1 = "images/dice" + rndNumber1 + ".png";
-let random2 = "images/dice" + rndNumber2 + ".png";
+let random2 = "images/dice" + rndNumber1 + ".png";
 
 document.querySelector(".img1").setAttribute("src", random1);
 document.querySelector(".img2").setAttribute("src", random2);
